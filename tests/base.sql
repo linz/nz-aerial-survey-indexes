69e9d3db53eb09a2ca5e241bbe7dd6b252d9499d
Daniel Silk <dsilk@linz.govt.nz>
2018-01-19 11:20:13 +1300

    Remove duplicate test setting + more comments

--- tests/base.sql
+++ tests/base.sql
@@ -13,18 +13,17 @@
 -- Revert all changes on failure.
 \set ON_ERROR_ROLLBACK 1
 \set ON_ERROR_STOP true
-\set QUIET 1
 
 
 BEGIN;
 
 SELECT plan(1);
 
-
+-- Tests
 SELECT has_schema('aerial');
 SELECT has_table('aerial', 'imagery_surveys', 'Should have imagery surveys table in the aerial schema');
 
-
+-- Finish pgTAP testing
 SELECT * FROM finish();
 
 ROLLBACK;
