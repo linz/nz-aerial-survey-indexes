5a1d007ffcb941172fd6523fbed71283e0350478
Rebecca <rclarke@linz.govt.nz>
2020-03-16 13:26:57 +1300

    feat: removed row and column

--- sql/04-generate_tile_index.sql
+++ sql/04-generate_tile_index.sql
@@ -8,25 +8,31 @@ CREATE OR REPLACE FUNCTION aerial_lds.generate_tile_index(
 	OUT out_sheet_code_id character varying,
 	OUT out_tile_scale integer,
 	OUT out_tile character varying,
-	OUT out_row_num integer,
-	OUT out_column_num integer,
 	OUT out_shape geometry)
     RETURNS SETOF record 
     LANGUAGE 'plpgsql'
 
     COST 100
-    VOLATILE 
+    STABLE 
     ROWS 1000
 AS $BODY$
 DECLARE
 	tile_count integer;
+	tile_length integer;
 BEGIN
 	
 	-- Script to create large scale index tile datasets
-	-- Retiles the 1:50k Tiles based on an inputted scale		
-
+	-- Retiles the 1:50k Tiles based on an inputted scale	
+	
+	-- Ensure tile scale is a multiple of 50k
+	IF mod(50000,in_tile_scale) != 0 THEN
+		RAISE EXCEPTION 'Scale is not a multiple of 50,000 --> %', in_tile_scale
+      	USING HINT = 'Check if a whole number is created when diving 50,000 and the inputted scale';
+	END IF;
+	
 	tile_count = 50000 / in_tile_scale;	
-			
+	IF tile_count > 99 THEN tile_length = 3; ELSE tile_length = 2; END IF;
+				
 	RETURN QUERY
 		--preprepare the 50k tiles, creating a column containing the width and height of the new tiles
 		WITH nz_50k AS (
@@ -43,8 +49,6 @@ BEGIN
 			nz_50k.sheet_code_id,
 			in_tile_scale,
 			tile.new_tile,
-			rw,
-			col,
 			tile.new_geom	
 		FROM 
 			generate_series(1,tile_count) col
@@ -52,12 +56,13 @@ BEGIN
 			CROSS JOIN nz_50k			
 			CROSS JOIN LATERAL (
 				SELECT
-				 	-- Concatenate the row and column together to create a string to identify the tile with
+				 	-- Concatenate the row and column together to create a string to identify the tile with. 
+					-- It will be padded with zeros where the character length is less than tile_length.
 					concat(
-						CASE WHEN char_length(rw::character varying) = 1 THEN '0'|| rw::character varying ELSE rw::character varying END,
-						CASE WHEN char_length(col::character varying) = 1 THEN '0'|| col::character varying ELSE col::character varying END
-					)::character varying as new_tile,
-
+						lpad(rw::character varying, tile_length, '0')::character varying,
+						lpad(col::character varying, tile_length, '0')::character varying 
+					)::character varying AS new_tile,
+				
 					-- Create a new tile. Its location is determined by the current row and column.	
 					ST_Multi(ST_MakeEnvelope( 
 						ST_XMin(nz_50k.shape) + (new_tile_width * col),				 
@@ -65,7 +70,7 @@ BEGIN
 						ST_XMin(nz_50k.shape) + (new_tile_width * (col-1)), 				
 						ST_YMax(nz_50k.shape) - (new_tile_height * (rw-1)),
 						2193						 						 
-					)) as new_geom
+					)) AS new_geom
 			) tile;
 	
 END;
